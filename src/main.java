6726d27ad1ab0d5084b89f82408270bd6216de72
Namnextx <phuongnam.nextx@gmail.com>
2019-02-19 16:22:06 +0700

    1. create class main

---
+++ src/main.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+////        System.out.print("Moi ban nhap vao a:");
+//        int a = sc.nextInt();
+////        System.out.print("Moi ban nhap vao b:");
+//        int b=sc.nextInt();
+//        int sum=a+b;
+//        System.out.println("Sum of "+a+" Va "+b+" = "+sum);
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập vào tên người yêu cũ  : ");
+//        String mylove_old = input.nextLine();
+//        System.out.println("Tên người yêu cũ : "+mylove_old);
+        Date date=new Date();
+        System.out.print("Thoi gian cua he thong la; "+date);
+    }
+}
